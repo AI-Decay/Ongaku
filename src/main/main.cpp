9c863a627fed8d217bcbfd09adfc8f169189eb18
Remez Pavel <github.aidecay@gmail.com>
2021-07-11 03:21:05 +0300

    *Move init to launcher

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,29 +1,6 @@
-#include "song_list_model/song_list_model.h"
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include "core/launcher/launcher.h"
 
 int main(int argc, char *argv[]) {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
-
-  QGuiApplication app(argc, argv);
-
-  list_model::song_list_model song_model;
-  song_model.load_songs_from_direcory("E:/Google Download");
-  QQmlApplicationEngine engine;
-  qmlRegisterSingletonInstance("Models.SongList", 1, 0, "SongListModel",
-                               &song_model);
-
-  const QUrl url(QStringLiteral("qrc:/ui/ongaku.qml"));
-  QObject::connect(
-      &engine, &QQmlApplicationEngine::objectCreated, &app,
-      [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-          QCoreApplication::exit(-1);
-      },
-      Qt::QueuedConnection);
-  engine.load(url);
-
-  return app.exec();
+  const auto launcher = core::launcher::сreate(argc, argv);
+  return launcher ? launcher->exec() : 1;
 }
